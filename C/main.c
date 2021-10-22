bd34ac85650424f48121567e4a5e88d5382c28cc
Cal <huskusocial@gmail.com>
2021-10-22 15:45:43 +0200

    v

--- C/main.c
+++ C/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main ()
 {
@@ -6,4 +6,74 @@ printf ("Bonjour");
    getchar ();
 printf ("Merci d'avoir appuyé sur une touche, Appuie à nouveau sur une touche pour quitter le programme");
    return 0;
+}
+*/
+/*
+#include <stdio.h>
+
+int main ()
+{
+int i;
+   int j;
+   i=22;
+   j=i;
+   printf ("i vaut %d\n", i);
+   printf ("i+j vaut %d\n", i+j);
+getchar ();
+   return 0;
+}
+*/
+
+
+/*
+#include <stdio.h>
+int main ()
+{
+char car;
+   char f;
+car='E';
+   f='e';
+   printf("car=%c f=%c\n",car,f);
+getchar ();
+return 0;
+}
+*/
+/*
+int main ()
+{
+    int i;
+    i='U';
+    printf("Le caractere %d est %c",i,i);
+    getchar();
+    return 0;
+}
+*/
+/*
+#include <stdio.h>
+int main ()
+{
+    printf("Calculatrice :\n\n");
+    printf("Valeur de a :\n");
+    int a;
+    a=36;
+    getchar();
+    printf("Valeur de b :\n");
+    int b;
+    b=54;
+    getchar();
+    printf("Valeur de a + b :\n%d", a+b);
+}
+*/
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf ("Valeur de a :\n");
+scanf ("%d", &a);
+    getchar();
+    printf("Valeur de b:\n");
+    int b;
+    scanf("%d", &b);
+    printf("Valeur de a + b :\n%d", a+b);
+
 }
