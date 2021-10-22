482e97a87bfa9772fc5288a23dca08a4c20efc90
Cal <huskusocial@gmail.com>
2021-10-22 15:39:28 +0200

    f
    yeah

---
+++ C/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main ()
+{
+printf ("Bonjour");
+   getchar ();
+printf ("Merci d'avoir appuyé sur une touche, Appuie à nouveau sur une touche pour quitter le programme");
+   return 0;
+}
